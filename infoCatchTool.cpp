a2b29b5bc31a4cf0aaa98ab5cc5a96772f4b4ac5
hanhao <hanhao@cisco.com>
2018-03-06 22:13:09 +0800

    version 1

--- infoCatchTool.cpp
+++ infoCatchTool.cpp
@@ -9,6 +9,7 @@
 #include<string>
 #include<vector>
 #include<memory>
+#include<fstream>
 using namespace std;
 
 void getInfo(vector<string> &v){
@@ -34,10 +35,44 @@ const char* string2char(string s){
     return p->c_str();
 }
 
+void getFilename(string &sFilename, const vector<string> &v){
+    auto i = v.end() - 1;
+    sFilename = *i;
+    cout << "filename is : " << sFilename << endl;
+}
+
+bool searchStrInLine(vector<string> v, string sLine){
+    auto i = v.begin();
+    while(v.end() - 1 != i){
+        if(string::npos == sLine.find(*i)){
+            return 0;
+        } 
+        ++i;
+    }
+    return 1;
+}
+
 int main(){
     vector<string> vInfo;
-    cout << "Please input vector" << endl;
     getInfo(vInfo);
-    printVector(vInfo);
+    // At least 2 string; 
+    // the last one is name of the file which you want to search in;
+
+    string sFilename = "";
+    getFilename(sFilename, vInfo);
+    // got filename
+
+    ifstream file;
+    file.open(sFilename, ios::in);
+    if(!file.is_open()){
+        cout << "file open error ! " << endl;
+        return 0;
+    }
+    string sLine = "";
+    while(getline(file, sLine)){
+        if(searchStrInLine(vInfo, sLine)){
+            cout << sLine << endl;
+        }
+    }
     return 0;
 }
