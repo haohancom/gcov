45f3c4c5c78a3a2534360567db0efda3e811b807
Han Hao <haohancom@126.com>
2018-04-10 06:38:14 +0000

    build a class

--- diffAnalyse.cpp
+++ diffAnalyse.cpp
@@ -21,8 +21,8 @@ void res2int(vector<string> vDiffReslut, vector<int> &vChangedLine){
         if ('@' == str[0] && '@' == str[1]){
             begin = 1;
 
-            std::size_t pos1= str.find("+");
-            std::size_t pos2= str.find(",", pos1 + 1);
+            std::size_t pos1 = str.find("+");
+            std::size_t pos2 = str.find(",", pos1 + 1);
             sPos = str.substr(pos1 + 1, pos2 - pos1 - 1);
             str2int(sPos, iPos);
             --iPos; //now iPos saves the begin position of changed code
@@ -47,6 +47,67 @@ void res2int(vector<string> vDiffReslut, vector<int> &vChangedLine){
     }
 }
 
+class result{
+public:
+    static result& instance(vector<string>::const_iterator it){
+        static result RESULT(it);
+        return RESULT;
+    }
+
+    void nextit() {++it;}
+    void it2str() {str = *it;}
+    const char str0() const {return str[0];}
+
+    void End();
+    void Begin();
+    void Normal();
+    void Add();
+    void Minus();
+
+private:
+    bool begin = 0;
+    string str = "";
+    string sPos = "";
+    int iPos = 0;
+    vector<string>::const_iterator it ;
+    vector<int> vChangedLine;
+
+    result(vector<string>::const_iterator it){
+        this->it = it;
+    }
+
+    result(result const&);
+    result& operator=(result const&);
+    ~result();
+};
+
+void result::End(){
+    begin = 0;
+    iPos = 0;
+}
+
+void result::Begin(){
+    begin = 1;
+    std::size_t pos1 = str.find("+");
+    std::size_t pos2 = str.find(",", pos1 + 1);
+    sPos = str.substr(pos1 + 1, pos2 - pos1 - 1);
+    str2int(sPos, iPos);
+    --iPos;
+}
+
+void result::Normal(){
+    ++iPos;   
+}
+
+void result::Add(){
+    ++iPos;
+    vChangedLine.push_back(iPos);
+}
+
+void result::Minus(){
+    //do nothing
+}
+
 int main(){
     vector<string> vDiffReslut;;   
     shellResultToVector(shell("git diff"), vDiffReslut);
@@ -54,5 +115,7 @@ int main(){
     //analyse vDiffReslut
     vector<int> vChangedLine;
     res2int(vDiffReslut, vChangedLine);
+
+    showVec(vChangedLine);
     return 0;
 }
